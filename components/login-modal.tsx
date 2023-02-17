f8105481ce3e6d9be5eee23d525543174a1e82a9
Nanda Syahrasyad <nanda.s@hey.com>
2023-02-16 18:17:15 -0800

    feat: use animate presence

--- components/login-modal.tsx
+++ components/login-modal.tsx
@@ -50,87 +50,96 @@ export function LoginModal() {
               Sign up
             </TitleToggle>
           </Dialog.Title>
-          <div className="-mx-10 px-10 overflow-hidden">
-            <motion.div
-              className="flex gap-20"
-              animate={{ x: isLogin ? 0 : -400 }}
-              transition={{ type: "spring", damping: 20 }}
-            >
-              <form
-                className="w-full relative group overflow-hidden py-[1px] shrink-0"
-                onSubmit={(evt) => {
-                  evt.preventDefault();
-                  submit();
-                }}
-              >
-                <label
-                  htmlFor="login-email"
-                  className="text-gray10 text-sm mb-2 block"
+          <div className="-mx-10 px-10 overflow-hidden relative">
+            <AnimatePresence mode="popLayout">
+              {isLogin ? (
+                <motion.form
+                  key="login"
+                  initial={{ x: -400 }}
+                  animate={{ x: 0 }}
+                  exit={{ x: -400 }}
+                  transition={{ type: "spring", damping: 20 }}
+                  className="w-full relative group overflow-hidden py-[1px] shrink-0"
+                  onSubmit={(evt) => {
+                    evt.preventDefault();
+                    submit();
+                  }}
                 >
-                  Email
-                </label>
-                <input
-                  id="login-email"
-                  type="email"
-                  className="block w-full p-2 border rounded-[4px] border-gray6 placeholder:text-gray8 focus:outline-gray10 disabled:text-gray8 mb-4 bg-gray2"
-                  placeholder="john@example.com"
-                  disabled={state === "loading"}
-                />
-                <label
-                  htmlFor="login-password"
-                  className="text-gray10 text-sm mb-2 block"
+                  <label
+                    htmlFor="login-email"
+                    className="text-gray10 text-sm mb-2 block"
+                  >
+                    Email
+                  </label>
+                  <input
+                    id="login-email"
+                    type="email"
+                    className="block w-full p-2 border rounded-[4px] border-gray6 placeholder:text-gray8 focus:outline-gray10 disabled:text-gray8 mb-4 bg-gray2"
+                    placeholder="john@example.com"
+                    disabled={state === "loading"}
+                  />
+                  <label
+                    htmlFor="login-password"
+                    className="text-gray10 text-sm mb-2 block"
+                  >
+                    Password
+                  </label>
+                  <input
+                    id="login-password"
+                    type="password"
+                    className="block w-full p-2 border rounded-[4px] border-gray6 placeholder:text-gray8 focus:outline-gray10 disabled:text-gray8 mb-6 bg-gray2"
+                    placeholder="john1234"
+                    disabled={state === "loading"}
+                  />
+                  <button className="block bg-gray4 w-full p-2 border border-gray6 rounded-[4px] text-gray11 text-sm focus:outline-gray10">
+                    Login
+                  </button>
+                </motion.form>
+              ) : (
+                <motion.form
+                  key="signup"
+                  initial={{ x: 400 }}
+                  animate={{ x: 0 }}
+                  exit={{ x: 400 }}
+                  transition={{ type: "spring", damping: 20 }}
+                  className="w-full relative group overflow-hidden py-[1px] shrink-0"
+                  onSubmit={(evt) => {
+                    evt.preventDefault();
+                    submit();
+                  }}
                 >
-                  Password
-                </label>
-                <input
-                  id="login-password"
-                  type="password"
-                  className="block w-full p-2 border rounded-[4px] border-gray6 placeholder:text-gray8 focus:outline-gray10 disabled:text-gray8 mb-6 bg-gray2"
-                  placeholder="john1234"
-                  disabled={state === "loading"}
-                />
-                <button className="block bg-gray4 w-full p-2 border border-gray6 rounded-[4px] text-gray11 text-sm focus:outline-gray10">
-                  Login
-                </button>
-              </form>
-              <form
-                className="w-full relative group overflow-hidden py-[1px] shrink-0"
-                onSubmit={(evt) => {
-                  evt.preventDefault();
-                  submit();
-                }}
-              >
-                <label
-                  htmlFor="login-email"
-                  className="text-gray10 text-sm mb-2 block"
-                >
-                  Email
-                </label>
-                <input
-                  id="login-email"
-                  type="email"
-                  className="block w-full p-2 border rounded-[4px] border-gray6 placeholder:text-gray8 focus:outline-gray10 disabled:text-gray8 mb-4 bg-gray2"
-                  placeholder="john@example.com"
-                  disabled={state === "loading"}
-                />
-                <label
-                  htmlFor="login-password"
-                  className="text-gray10 text-sm mb-2 block"
-                >
-                  Password
-                </label>
-                <input
-                  id="login-password"
-                  type="password"
-                  className="block w-full p-2 border rounded-[4px] border-gray6 placeholder:text-gray8 focus:outline-gray10 disabled:text-gray8 mb-6 bg-gray2"
-                  placeholder="john1234"
-                  disabled={state === "loading"}
-                />
-                <button className="block bg-gray4 w-full p-2 border border-gray6 rounded-[4px] text-gray11 text-sm focus:outline-gray10">
-                  Sign up
-                </button>
-              </form>
-            </motion.div>
+                  <label
+                    htmlFor="login-email"
+                    className="text-gray10 text-sm mb-2 block"
+                  >
+                    Email
+                  </label>
+                  <input
+                    id="login-email"
+                    type="email"
+                    className="block w-full p-2 border rounded-[4px] border-gray6 placeholder:text-gray8 focus:outline-gray10 disabled:text-gray8 mb-4 bg-gray2"
+                    placeholder="john@example.com"
+                    disabled={state === "loading"}
+                  />
+                  <label
+                    htmlFor="login-password"
+                    className="text-gray10 text-sm mb-2 block"
+                  >
+                    Password
+                  </label>
+                  <input
+                    id="login-password"
+                    type="password"
+                    className="block w-full p-2 border rounded-[4px] border-gray6 placeholder:text-gray8 focus:outline-gray10 disabled:text-gray8 mb-6 bg-gray2"
+                    placeholder="john1234"
+                    disabled={state === "loading"}
+                  />
+                  <button className="block bg-gray4 w-full p-2 border border-gray6 rounded-[4px] text-gray11 text-sm focus:outline-gray10">
+                    Sign up
+                  </button>
+                </motion.form>
+              )}
+            </AnimatePresence>
           </div>
           <Dialog.Close asChild>
             <button className="absolute -top-5 -right-5 flex items-center justify-center w-10 h-10 bg-gray3 rounded-full border border-gray5 shadow-md">
