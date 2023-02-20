4c55cc703197319460158af54012d7a27c85c08a
Nanda Syahrasyad <nanda.s@hey.com>
2023-02-19 17:36:25 -0800

    feat: improve progress load (#6)

--- app/kanji/[level]/kanji-item.tsx
+++ app/kanji/[level]/kanji-item.tsx
@@ -3,39 +3,29 @@
 import React from "react";
 import { motion } from "framer-motion";
 import type { Kanji } from "@/lib/kanji";
-import { useSupabase } from "@/app/supabase";
 import cn from "classnames";
 
 export const KanjiItem = ({
   kanji,
   level,
+  proficiency,
   ...props
 }: {
   kanji: Kanji;
   level: number;
+  proficiency: number | undefined;
 } & Omit<React.ComponentPropsWithoutRef<"button">, "href">) => {
-  const { session } = useSupabase();
-  const [proficiency, setProficiency] = React.useState(0);
-
-  React.useEffect(() => {
-    if (!session) {
-      setProficiency(0);
-      return;
-    }
-    fetch(`/api/proficiency/kanji/${kanji.literal}?level=${level}`)
-      .then((res) => res.json())
-      .then(({ proficiency }) => setProficiency(proficiency / 3));
-  }, [session, level, kanji.literal]);
-
+  const _proficiency = (proficiency ?? 0) / 3;
   return (
     <li>
       <button
         className={cn(
           "w-24 h-24 text-5xl font-bold rounded-md flex items-center justify-center bg-gray2 border border-gray4 relative overflow-hidden hover:border-gray6 hover:bg-gradient-to-b hover:from-gray5 hover:to-gray3 focus:outline-none focus-visible:border-2 focus-visible:border-gray8",
-          proficiency < 1 ? "shadow-md" : "shadow-gray4/60 border-gray6"
+
+          _proficiency < 1 ? "shadow-md" : "shadow-gray4/60 border-gray6"
         )}
         style={
-          proficiency === 1
+          _proficiency === 1
             ? { boxShadow: "0 0 10px 5px var(--tw-shadow-color)" }
             : undefined
         }
@@ -44,7 +34,7 @@ export const KanjiItem = ({
         <motion.span
           className="absolute inset-0 bg-gray4"
           style={{ originY: "bottom" }}
-          animate={{ scaleY: proficiency }}
+          animate={{ scaleY: _proficiency }}
           initial={{ scaleY: 0 }}
           transition={{ type: "spring", damping: 20 }}
         />
