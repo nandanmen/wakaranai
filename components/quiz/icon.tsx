077807c4be33ba217453aff3ad0b5c812fa4f1b9
Nanda Syahrasyad <nanda.s@hey.com>
2023-02-21 17:14:36 -0800

    chore: remove unused code

--- components/quiz/icon.tsx
+++ components/quiz/icon.tsx
@@ -11,11 +11,6 @@ export const Icon = ({
   "type"
 >) => {
   if (!type) return null;
-  const iconMap: Record<Type, string> = {
-    correct: "M 4 8 L 7 10.8 L 12 4",
-    incorrect: "M 3 3 L 12 12 M 3 12 L 12 3",
-    skipped: "M 2.8 7.5 H 12.2",
-  };
   return (
     <motion.div className="absolute bottom-3 right-2">
       <IconOnly type={type} size={25} />
