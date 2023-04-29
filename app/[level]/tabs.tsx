c789cb365e1ca91c54822d44656e41b90f2baa3b
Nanda Syahrasyad <nanda.s@hey.com>
2023-04-29 11:54:24 -0700

    feat: disable kanji/stories list

--- app/[level]/tabs.tsx
+++ app/[level]/tabs.tsx
@@ -4,6 +4,8 @@ import { TABS } from "@/lib/constants";
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 
+const notAvailable = ["Kanji", "Stories"];
+
 export function Tabs() {
   const params = useParams();
   const pathname = usePathname();
@@ -11,6 +13,17 @@ export function Tabs() {
   return (
     <ul className="flex bg-gray-1 p-1 border border-gray-6 rounded-full w-fit">
       {TABS.map((tab) => {
+        if (notAvailable.includes(tab))
+          return (
+            <li>
+              <p className="px-2 py-1 rounded-full text-gray-11 flex items-center gap-1">
+                <span>{tab}</span>
+                <span className="rounded-md border border-gray-6 text-sm leading-none p-1">
+                  soon
+                </span>
+              </p>
+            </li>
+          );
         return (
           <li key={tab}>
             <Link
